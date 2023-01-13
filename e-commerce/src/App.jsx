8af5d01915d816516292a0a6d84d99800ee3b27c
k-amhaz <amhaz.dev@gmail.com>
2023-01-13 22:14:22 +0200

    Footer added

--- e-commerce/src/App.jsx
+++ e-commerce/src/App.jsx
@@ -3,7 +3,11 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Footer />
+    </div>
+  );
 }
 
 export default App;
--- e-commerce/src/components/Footer.jsx
+++ e-commerce/src/components/Footer.jsx
@@ -1,5 +1,59 @@
 import React from "react";
+import { Container } from "@mui/material";
+import "../css/Footer.css";
 
 export default function Footer() {
-  return <div>Footer</div>;
+  return (
+    <>
+      <div className="footer">
+        <Container className="footer-links">
+          <div className="footer-info">
+            <h3 className="footer-title">Hekto</h3>
+            <div className="footer-phone-number footer-contact">
+              <i className="fa-solid fa-phone"></i>
+              <p>+1 (1234) 56789</p>
+            </div>
+            <div className="footer-email footer-contact">
+              <i className="fa-solid fa-envelope"></i>
+              <p>example@kocode.contact</p>
+            </div>
+          </div>
+          <div className="footer-pages">
+            <h5>Pages</h5>
+            <p>Contact us</p>
+            <p>About us</p>
+            <p>F&Q</p>
+            <p>Shop</p>
+            <p>Cart</p>
+            <p>Favorites</p>
+          </div>
+          <div className="footer-categories">
+            <h5>Categories</h5>
+            <p>Electronics</p>
+            <p>Jewelry</p>
+            <p>Men's clothing</p>
+            <p>Woman's clothing</p>
+          </div>
+
+          <div className="footer-account">
+            <h5>Account</h5>
+            <p>Sign up</p>
+            <p>Log in</p>
+          </div>
+        </Container>
+        <footer>
+          <div className="footer-bottom-container">
+            <p>
+              <span>KO code</span> © 2023
+            </p>
+            <div className="footer-icons">
+              <i className="fa-brands fa-instagram"></i>
+              <i className="fa-brands fa-linkedin"></i>
+              <i className="fa-brands fa-twitter"></i>
+            </div>
+          </div>
+        </footer>
+      </div>
+    </>
+  );
 }
