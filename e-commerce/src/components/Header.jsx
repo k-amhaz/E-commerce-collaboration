0ea4335dadae6d03a784cf085445c6e6d2bb974d
ali-allaw <aliodin2k18@gmail.com>
2023-01-13 13:12:34 +0200

    header files has been uploaded

---
+++ e-commerce/src/components/Header.jsx
@@ -0,0 +1,11 @@
+import '../css/Header.css'
+
+const Header = () => {
+    return ( 
+        <div className="header">
+            <h1>This is the header page</h1>
+        </div>
+     );
+}
+ 
+export default Header;
