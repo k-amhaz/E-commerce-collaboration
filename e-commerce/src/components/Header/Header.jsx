c4398e869d47cdfb70675db0b9a80e5c8157bdf2
ali-allaw <aliodin2k18@gmail.com>
2023-01-14 22:13:49 +0200

    header has been upgraded

--- e-commerce/src/components/Header/Header.jsx
+++ e-commerce/src/components/Header/Header.jsx
@@ -7,23 +7,27 @@ const Header = () => {
     <>
         <div className="header">
             <div className="up-header ">
-                <div className='container-lg up-header-container d-flex justify-content-between align-items-center'>
-                    <div className="left d-flex justify-content-center  align-items-center gap-5">
+                <div className='container-lg up-header-container  d-flex justify-content-start justify-content-md-between align-items-center'>
+                    <div className="left d-none d-md-flex justify-content-center  align-items-center gap-5">
                         <div className="left-mail d-flex justify-content-center  align-items-center gap-2">
                             <i className="bi bi-envelope"></i>
                             <p>example@gmail.com</p>
                         </div>
-                        <div className="left-phone d-flex justify-content-center  align-items-end gap-2">
+                        <div className="left-phone d-flex  justify-content-center  align-items-end gap-2">
                             <i className="bi bi-telephone-plus "></i>
                             <p>(123456) 7800</p>
                         </div>
                     </div>
                     <div className="right d-flex justify-content-center  align-items-center gap-2">
                         <div className="right-menu d-flex justify-content-center  align-items-end gap-2">
-                            <p>Login</p>
-                            <i className="bi bi-person"></i>
-                            <p>Wishlist</p>
-                            <i className="bi bi-heart"></i>
+                            <div className="right-menu-login d-flex justify-content-center  align-items-center gap-2">
+                                <p>Login</p>
+                                <i className="bi bi-person"></i>
+                            </div>
+                            <div className="right-menu-fav d-flex justify-content-center  align-items-center gap-2">
+                                <p>Wishlist</p>
+                                <i className="bi bi-heart"></i>
+                            </div>
                         </div>
                         <i className="bi bi-cart ms-4"></i>
                     </div>
