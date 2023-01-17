cf7d92a2bc1c0298308ba3ebd65f3a96a7d15cbb
k-amhaz <amhaz.dev@gmail.com>
2023-01-17 02:58:46 +0200

    Navbar changed to categories

--- e-commerce/src/components/Header/Navbar.jsx
+++ e-commerce/src/components/Header/Navbar.jsx
@@ -28,7 +28,7 @@ const Navbar = () => {
               </li>
               <li className="nav-item">
                 <a className="nav-link" href="#">
-                  Products
+                  Categories
                 </a>
               </li>
               <li className="nav-item">
