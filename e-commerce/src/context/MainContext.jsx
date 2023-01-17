824e12e89b8b887eb2c6b9166467ad335866fff6
k-amhaz <amhaz.dev@gmail.com>
2023-01-17 16:47:24 +0200

    nothing fuckn important

--- e-commerce/src/context/MainContext.jsx
+++ e-commerce/src/context/MainContext.jsx
@@ -6,14 +6,11 @@ const {Provider, Consumer} = Context;
 const ContextProvider = (props) => {
 
     const [data, setData] = useState([])
-<<<<<<< HEAD
 
 
     for (let i = 0; i < img.length; i++) {
         img .push()
     }
-=======
->>>>>>> 40fe456e66f668c8a6574e49434ac48f238ebbac
 
     useEffect(() => {
         fetch('https://fakestoreapi.com/products')
@@ -22,14 +19,12 @@ const ContextProvider = (props) => {
     }, [])
 
     return (
-<<<<<<< HEAD
         <Provider value={data}>
-=======
+
         <Provider value={{data}}>
->>>>>>> 40fe456e66f668c8a6574e49434ac48f238ebbac
             {props.children}
         </Provider>
-    );
-}
+    )
+    };
  
 export {ContextProvider, Context};
